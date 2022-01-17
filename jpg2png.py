c2d20086e0b81b422f1dc7cbbdefb59def7dbd00
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2022-01-17 22:06:18 +0530

    Create jpg2png.py

---
+++ jpg2png.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+print(image_folder, output_folder)
+# check if new folder exists, if not create it
+
+# loop through pokedex
+
+# convert images to png
+
+# save to the new folder
