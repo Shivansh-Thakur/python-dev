7a68e4b0b03fe48b3f56326065df78616244d2cc
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2022-01-21 20:58:37 +0530

    Update jpg2png.py

--- jpg2png.py
+++ jpg2png.py
@@ -6,11 +6,18 @@ from PIL import Image
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
-print(image_folder, output_folder)
 # check if new folder exists, if not create it
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-# loop through pokedex
+# loop through pokedex folder
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
 
-# convert images to png
+    # convert images to png and save to the new folder
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('all done!')
 
-# save to the new folder
+
+ 
