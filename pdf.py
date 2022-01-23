f57eaf8789441fff00c2cd3ff8bb0a0518b639f2
Shivansh Thakur <56513036+Shivansh-Thakur@users.noreply.github.com>
2022-01-23 23:19:15 +0530

    Create pdf.py

---
+++ pdf.py
@@ -0,0 +1,11 @@
+import PyPDF2
+
+with open('dummy.pdf', 'rb') as file:
+    reader = PyPDF2.PdfFileReader(file)
+    page = reader.getPage(0)
+    page.rotateCounterClockwise(90)
+    
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+    with open('tilt.pdf', 'wb') as new_file:
+        writer.write(new_file)
